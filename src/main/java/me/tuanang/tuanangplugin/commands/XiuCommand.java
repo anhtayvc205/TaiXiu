130a78e09644f76b0e31ec44d328372c971dace5
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-26 21:11:28 +0700

    Create XiuCommand.java

---
+++ src/main/java/me/tuanang/tuanangplugin/commands/XiuCommand.java
@@ -0,0 +1,13 @@
+package me.tuanang.tuanangplugin.commands;
+
+public class XiuCommand extends BetCommand {
+
+    public XiuCommand() {
+        super();
+    }
+
+    @Override
+    protected boolean isTai() {
+        return false;
+    }
+}
