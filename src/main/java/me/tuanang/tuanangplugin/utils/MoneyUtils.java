fd907796a85e5c6767c86c5f14a718e87957a2c1
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-26 21:47:16 +0700

    Create MoneyUtils.java

---
+++ src/main/java/me/tuanang/tuanangplugin/utils/MoneyUtils.java
@@ -0,0 +1,33 @@
+package org.kazamistudio.taiXiuPlugin.utils;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class MoneyUtils {
+
+    public MoneyUtils() {
+    }
+
+    public static String formatVietMoney(double amount) {
+
+        // >= 100 triệu
+        if (amount >= 1.0E8) {
+            double ty = amount / 1.0E9;
+
+            DecimalFormat format = new DecimalFormat(
+                    "#,###.##",
+                    new DecimalFormatSymbols(new Locale("vi", "VN"))
+            );
+
+            return format.format(ty) + " Tỷ";
+        }
+
+        // < 100 triệu
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setGroupingSeparator('.');
+
+        DecimalFormat format = new DecimalFormat("#,###", symbols);
+        return format.format(amount);
+    }
+}
