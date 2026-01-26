ef5bfa9fea7270f072b3a80a3a49d881f9a80bdc
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-26 21:09:12 +0700

    Create TaiCommand.java

---
+++ src/main/java/me/tuanang/tuanangplugin/commands/TaiCommand.java
@@ -0,0 +1,13 @@
+package me.tuanang.tuanangplugin.commands;
+
+public class TaiCommand extends BetCommand {
+
+    public TaiCommand() {
+        super();
+    }
+
+    @Override
+    protected boolean isTai() {
+        return true;
+    }
+}
