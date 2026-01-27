5dd18b78f0bfc14394ae8339816b3b13d1a6d0c7
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-27 07:13:24 +0700

    Create XiuCommand.java

---
+++ src/main/java/me/tuanang/taixiuplugin/commands/XiuCommand.java
@@ -0,0 +1,33 @@
+package me.tuanang.taixiuplugin.commands;
+
+import me.tuanang.taixiuplugin.*;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+
+public class XiuCommand implements CommandExecutor {
+
+    private final TaiXiuPlugin plugin;
+
+    public XiuCommand(TaiXiuPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
+        if (!(s instanceof Player p)) return true;
+        if (a.length == 0) return false;
+
+        double money = Double.parseDouble(a[0]);
+
+        if (!plugin.game().check(p, money)) return true;
+        if (!plugin.currency().has(p, money)) {
+            p.sendMessage("§cBạn không đủ tiền!");
+            return true;
+        }
+
+        plugin.currency().withdraw(p, money);
+        plugin.game().bet(p, "XIU", money);
+
+        p.sendMessage("§aĐã cược XỈU: §e" + money);
+        return true;
+    }
+}
