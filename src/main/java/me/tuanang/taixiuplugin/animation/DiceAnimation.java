651ff6a9692697252dcbb50529b60226f009a829
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-30 10:34:17 +0700

    Delete src/main directory

--- src/main/java/me/tuanang/taixiuplugin/animation/DiceAnimation.java
+++
@@ -1,9 +0,0 @@
-package me.tuanang.taixiuplugin.animation;
-
-import java.util.concurrent.ThreadLocalRandom;
-
-public class DiceAnimation {
-    public static int roll() {
-        return ThreadLocalRandom.current().nextInt(1, 7);
-    }
-}
